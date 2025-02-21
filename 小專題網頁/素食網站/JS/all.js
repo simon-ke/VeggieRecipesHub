3a94a0e3a34439c216abe0d0f16bb16c159d7c86
simon-ke <a40515041@gmail.com>
2025-02-21 20:30:55 +0800

    更新滾輪事件

---
+++ 小專題網頁/素食網站/JS/all.js
@@ -0,0 +1,41 @@
+window.onscroll = () => {
+    fixedNav();
+    scrollFunction();
+};
+
+// 固定導覽列
+const fixedNav = () => {
+    const nav = document.querySelector('.nav-container');
+    const search = document.querySelector('.search-container');
+
+    if (window.scrollY > 200) {
+        requestAnimationFrame(() => {
+            nav.classList.add('fixed-nav');
+            search.classList.add('fixed-nav-search');
+        });
+    } else {
+        requestAnimationFrame(() => {
+            nav.classList.remove('fixed-nav');
+            search.classList.remove('fixed-nav-search');
+        });
+    }
+}
+
+// 當頁面滾動時觸發
+const scrollFunction = () => {
+    const backToTopButton = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+}
+
+
+// 點擊圖片回到頂部
+document.getElementById("back-to-top").onclick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+
+
